1dddfc2b6ec11b71cd7cffd30f40692d209c207a
ulisse <ulisse@ulisse.ulisse>
2022-04-15 15:44:45 -0500

    FIX: fix get location by id on location service

--- src/schemas/location.schema.ts
+++ src/schemas/location.schema.ts
@@ -1,10 +1,9 @@
 import * as mongoose from 'mongoose';
 
 export const LocationSchema = new mongoose.Schema({
-    _id: String,
-    name: String,
-    description: String,
-    coordinates: { latitude: Number, longitude: Number },
-    imageNames: Array,
-    link: String
+  name: String,
+  description: String,
+  coordinates: { latitude: Number, longitude: Number },
+  imageNames: Array,
+  link: String,
 });
