df857eb81b982580171499a4650bfa1d273ac299
ulisse <ulisse@ulisse.ulisse>
2022-04-15 16:42:44 -0500

    FEAT: add example for swagger

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateLocationDto {
-    @ApiProperty()
-    name: string;
-    @ApiProperty({ required: false })
-    description: string;
-    @ApiProperty()
-    coordinates: { latitude: number, longitude: number };
-    @ApiProperty({ required: false })
-    imageNames: Array<string>;
-    @ApiProperty({ required: false })
-    link: string;
+  @ApiProperty({ required: true, example: 'Duomo di Milano' })
+  name: string;
+  @ApiProperty({
+    required: true,
+    example: 'Il duomo si trova al centro di Milano',
+  })
+  description: string;
+  @ApiProperty({
+    required: true,
+    example: '{ "latitude": 1.0; "longitude": 2.0} ',
+  })
+  coordinates: { latitude: number; longitude: number };
+  @ApiProperty({ required: false, example: '["link_1.png", "link_2.png"]' })
+  imageNames: Array<string>;
+  @ApiProperty({ required: false, example: 'https://www.google.it' })
+  link: string;
 }
