fb9b98abbb36b0c394dab42a24313e23a5c6afdb
twopills <ceravolo.elia@gmail.com>
2022-04-15 13:02:50 -0500

    fix: add db module and location provider

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './service/locations.service';
 import { LocationsController } from './locations.controller';
+import { locationProviders } from './provider/location.provider';
+import { DatabaseModule } from 'src/config/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [LocationsController],
-  providers: [LocationsService]
+  providers: [LocationsService, ...locationProviders],
 })
 export class LocationsModule {}
