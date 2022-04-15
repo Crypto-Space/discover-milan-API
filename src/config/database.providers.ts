a6c420214a38d9aef47e2b95f4a94b4d0586ed0f
ulisse <ulisse@ulisse.ulisse>
2022-04-15 12:55:57 -0500

    FEAT: add env in database provider

--- src/config/database.providers.ts
+++ src/config/database.providers.ts
@@ -1,8 +1,12 @@
+import { env } from 'is_not_env';
 import * as mongoose from 'mongoose';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect(
+        `mongodb+srv://${env.DB_ADMIN}:${env.DB_PWD}@${env.DB_CLUSTER_NAME}.iufkn.mongodb.net/${env.DB_NAME}?retryWrites=true&w=majority`,
+      ),
   },
 ];
