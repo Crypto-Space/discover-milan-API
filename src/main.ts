2b3aadc6ed69fcbc3374f857f7f4689d151404a6
twopills <ceravolo.elia@gmail.com>
2022-04-16 16:27:21 -0500

    FEAT: add hot reloading

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Discover Milan API')
     .setDescription('The API for Discover Milan App')
-    .build()
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(3001);
+  await app.listen(3002);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
+
+/**
+ * declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
+ */
