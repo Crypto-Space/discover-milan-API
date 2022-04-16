1c68c24af3aa644aff8798daf4f68163473442a9
Mattia Cintura <mattiacintura@icloud.com>
2022-04-16 11:11:35 +0200

    WIP: update location

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { LocationDto } from './dto/location.dto';
+import { UpdateLocationDto } from './dto/update-location.dto';
 import { LocationsService } from './service/locations.service';
 
 @Controller('locations')
@@ -28,5 +29,13 @@ export class LocationsController {
   @ApiTags('Locations')
   async postLocation(@Body() input: CreateLocationDto): Promise<LocationDto> {
     return await this.locationsService.createLocation(input);
+  }
+
+  @ApiResponse({ type: LocationDto })
+  @ApiBody({ type: UpdateLocationDto })
+  @Put(':id')
+  @ApiTags('Locations')
+  async putLocation(@Param('id') id: string, @Body() input: UpdateLocationDto) {
+    return await this.locationsService.updateLocation(id, input);
   }
 }
