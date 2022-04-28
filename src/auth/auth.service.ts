b2a74bca37cf48279f896424fcd541f619d4a690
twopills <ceravolo.elia@gmail.com>
2022-04-28 12:37:10 -0500

    FEAT: add jwt + strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
@@ -13,4 +14,12 @@ export class AuthService {
     }
     return null;
   }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
 }
